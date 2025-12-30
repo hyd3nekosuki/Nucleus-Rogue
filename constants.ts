ec2f04497a665d9b941c9b4d1c2ba8ca9d5aa2fe
Tomohiro Endo <39391791+hyd3nekosuki@users.noreply.github.com>
2025-12-30 22:24:15 +0900

    fix: Correct spelling errors in element names
    Fixes several typos in the `ELEMENT_NAMES` array, including "Mnaganese" to "Manganese", "Cdmium" to "Cadmium", "Ceium" to "Cerium", and "Amerecium" to "Americium".

--- constants.ts
+++ constants.ts
@@ -1,3 +1,4 @@
+
 import { DecayMode, NuclideData, NuclideCategory } from './types';
 
 export const GRID_WIDTH = 15;
@@ -57,13 +58,13 @@ export const ELEMENT_SYMBOLS = [
 export const ELEMENT_NAMES = [
   "Neutron", "Hydrogen", "Helium", "Lithium", "Beryllium", "Boron", "Carbon", "Nitrogen", "Oxygen", "Fluorine", "Neon",
   "Sodium", "Magnesium", "Aluminium", "Silicon", "Phosphorus", "Sulfur", "Chlorine", "Argon", "Potassium", "Calcium",
-  "Scandium", "Titanium", "Vanadium", "Chromium", "Mnaganese", "Iron", "Cobalt", "Nickel", "Copper", "Zinc",
+  "Scandium", "Titanium", "Vanadium", "Chromium", "Manganese", "Iron", "Cobalt", "Nickel", "Copper", "Zinc",
   "Gallium", "Germanium", "Arsenic", "Selenium", "Bromine", "Krypton", "Rubidium", "Strontium", "Yttrium", "Zirconium",
-  "Niobium", "Molybdenum", "Technetium", "Ruthenium", "Rhodium", "Palladium", "Silver", "Cdmium", "Indium", "Tin",
-  "Antimony", "Tellurium", "Iodine", "Xenon", "Caesium", "Barium", "Lanthanum", "Ceium", "Praseodymium", "Neodymium",
+  "Niobium", "Molybdenum", "Technetium", "Ruthenium", "Rhodium", "Palladium", "Silver", "Cadmium", "Indium", "Tin",
+  "Antimony", "Tellurium", "Iodine", "Xenon", "Caesium", "Barium", "Lanthanum", "Cerium", "Praseodymium", "Neodymium",
   "Promethium", "Samarium", "Europium", "Gadolinium", "Terbium", "Dysprosium", "Holmium", "Erbium", "Thulium", "Ytterbium",
   "Lutetium", "Hafnium", "Tantalum", "Tungsten", "Rhenium", "Osmium", "Iridium", "Platinum", "Gold", "Mercury",
-  "Thallium", "Lead", "Bismuth", "Polonium", "Astatine", "Radon", "Francium", "Radium", "Actinium", "Thorium", "Protactinium", "Uranium", "Neptunium", "Plutonium", "Amerecium", "Curium", "Berkelium", "Californium", "Einsteinium", "Fermium",
+  "Thallium", "Lead", "Bismuth", "Polonium", "Astatine", "Radon", "Francium", "Radium", "Actinium", "Thorium", "Protactinium", "Uranium", "Neptunium", "Plutonium", "Americium", "Curium", "Berkelium", "Californium", "Einsteinium", "Fermium",
   "Mendelevium", "Nobelium", "Lawrencium", "Rutherfordium", "Dubnium", "Seaborgium", "Bohrium", "Hassium", "Meitnerium", "Darmstadtium", "Roentgenium", "Copernicium", "Nihonium", "Flerovium", "Moscovium", "Livermorium", "Tennessine", "Oganesson"
 ];
 
